411b60ea5d7925a49bd8f7c42dc9cbbbb9331099
Nathalie <nathalie.mancia@catolica.edu.sv>
2026-09-20 10:55:12 -0600

    Avance de pantallas y gestion de operadores

---
+++ PLANTILLATITAONOPS/PLANTILLATITAONOPS/src/main/java/com/mycompany/plantillatitaonops/PLANTILLATITAONOPS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.plantillatitaonops;
+
+/**
+ *
+ * @author aleyt
+ */
+public class PLANTILLATITAONOPS {
+
+    public static void main(String[] args) {
+        System.out.println("gogogo");
+    }
+}
